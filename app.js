4b3408d12e4e2b2bdef49077e41b410a6ad4af6c
Adem <adila78@hotmail.fr>
2021-06-11 10:57:36 +0200

    up

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const items=[ //création de la constante
+var items=[ //création de la constante
     {
         id:1,  // id du menu 1
         nom:"Menu Classic",  // nom du menu 1
-        img:"img/m1.png", //image de présention
+        img:"https://firebasestorage.googleapis.com/v0/b/ccf-hamadouche.appspot.com/o/m1.png?alt=media&token=ed3613a5-067a-4000-9182-0a948ac8cadc", //image de présention
         prix: 5,  //prix donné
         description:"Burger, Salade, Tomate, Cornichon" // description du burger
     
@@ -11,7 +11,7 @@ const items=[ //création de la constante
     {
         id:2,
         nom:"Menu Bacon",
-        img:"img/m2.png",
+        img:"https://firebasestorage.googleapis.com/v0/b/ccf-hamadouche.appspot.com/o/m2.png?alt=media&token=88f0bb0d-2bbe-405e-9869-2638eef996ba",
         prix: 6,
         description:"Burger, Fromage, Bacon, Salade, Tomate"
     
@@ -19,7 +19,7 @@ const items=[ //création de la constante
     {
         id:3,
         nom:"Menu Big",
-        img:"img/m3.png",
+        img:"https://firebasestorage.googleapis.com/v0/b/ccf-hamadouche.appspot.com/o/m3.png?alt=media&token=141af37e-061f-4425-8f62-ae4f2d2c7cc6",
         prix: 8,
         description:"Double Burger, Fromage, Cornichon, Salade"
     
@@ -27,7 +27,7 @@ const items=[ //création de la constante
     {
         id:4,
         nom:"Menu Chicken",
-        img:"img/m4.png",
+        img:"https://firebasestorage.googleapis.com/v0/b/ccf-hamadouche.appspot.com/o/m4.png?alt=media&token=f7680a51-bd36-47a3-a0e5-b9a977c2b5c8",
         prix: 7,
         description:"Poulet Frit, Tomate, Salade, Mayonnaise"
     
@@ -47,17 +47,12 @@ carte.addEventListener("click", function(){
     alert(doc.description);
 }) 
     }
+function vide(){
+list.innerHTML=""
+}
 
-
-function maxValue(doc) {
-    list.innerHTML=""
-    let carte = document.createElement('div'); // on créer une div 
-        carte.setAttribute('id', doc.id); // dans cette div il y aura l'id
-        carte.classList.add('card'); //et on lui rajoute la class card pour le css
-        carte.innerHTML='<div class="card_image"><img src="'+ doc.img + '"/></div><div class="card_title title-white"><div><p>'+ doc.nom+'</p><p>'+doc.prix +' E</p></div> </div> </div>'; // le block principal en html
- 
-carte.addEventListener("click", function(){
-    alert(doc.description);
-}) 
-    }
+function maxValue() {
+    vide();
+    console.log(carte);
+} 
 items.map(item=>afficheitem(item)); // on execute la fonction
